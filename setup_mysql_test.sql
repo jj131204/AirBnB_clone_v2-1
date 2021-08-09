064092bb22ebe38cd76839414bdde222833926ca
jj131204 <jj131204@gmail.com>
2021-08-09 21:20:11 +0000

    test3

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,8 @@
--- Create Database
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
---Next
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+-- Create a datbase
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create a new user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Next
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
