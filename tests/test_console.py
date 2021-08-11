7bed97b41a3762e1b502dc699bf964a864e4b4da
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-10 22:29:19 -0500

    test2

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,51 +1,93 @@
 #!/usr/bin/python3
+"""Console unittest
 """
-===============================================================================
-
-████████╗███████╗███████╗████████╗     ██████╗ █████╗ ███████╗███████╗███████╗
-╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝    ██╔════╝██╔══██╗██╔════╝██╔════╝██╔════╝
-   ██║   █████╗  ███████╗   ██║       ██║     ███████║███████╗█████╗  ███████╗
-   ██║   ██╔══╝  ╚════██║   ██║       ██║     ██╔══██║╚════██║██╔══╝  ╚════██║
-   ██║   ███████╗███████║   ██║       ╚██████╗██║  ██║███████║███████╗███████║
-   ╚═╝   ╚══════╝╚══════╝   ╚═╝        ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
-
-===============================================================================
-"""
-
-import console
-import inspect
-import pep8
 import unittest
+import os
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
+from models.engine.file_storage import FileStorage
+import inspect
+from unittest.mock import patch, create_autospec
+from io import StringIO
+from console import HBNBCommand
+import uuid
+from time import sleep
+import sys
+from models.__init__ import storage
 
-HBNBCommand = console.HBNBCommand
+type_storage = os.getenv('HBNB_TYPE_STORAGE')
 
 
-class TestConsoleDocs(unittest.TestCase):
-    """Class for testing documentation of the console"""
-    def test_pep8_conformance_console(self):
-        """Test that console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+class Test_console(unittest.TestCase):
+    """Test cases for the console.py file
+    """
+    clis = ['BaseModel', 'User', 'Place', 'City', 'Amenity', 'Review', 'State']
+    storage = FileStorage()
 
-    def test_pep8_conformance_test_console(self):
-        """Test that tests/test_console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def setUp(self):
+        """set environment to start testing"""
+        # Empty objects in engine
+        FileStorage._FileStorage__objects = {}
+        # Remove file.json if exists
+        if os.path.exists("file.json"):
+            os.remove("file.json")
 
-    def test_console_module_docstring(self):
-        """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
+    def tearDown(self):
+        """set enviroment when testing is finished"""
+        # Empty objects in engine
+        FileStorage._FileStorage__objects = {}
+        # Remove file.json if exists
+        if os.path.exists("file.json"):
+            os.remove("file.json")
 
-    def test_HBNBCommand_class_docstring(self):
-        """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs a docstring")
+    @unittest.skipIf(type_storage == 'db', "No apply for db")
+    def test_create(self):
+        """Test for create command
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            command = "create State name=\"California\""
+            HBNBCommand().onecmd(command)
+            _id = f.getvalue().strip()
+            key = "State" + "." + _id
+            self.assertTrue(key in storage.all().keys())
+
+    @unittest.skipIf(type_storage == 'db', "No apply for db")
+    def test_create2(self):
+        """Test for create command 2
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            state = State()
+            command = "create City name=\"Texas\" state_id=\"{}\"".format(
+                state.id)
+            HBNBCommand().onecmd(command)
+            _id = f.getvalue().strip()
+            key = "City" + "." + _id
+            nname = storage.all()[key].name
+            sid = storage.all()[key].state_id
+            self.assertTrue(key in storage.all().keys())
+            self.assertEqual('Texas', nname)
+            self.assertEqual(state.id, sid)
+
+    @unittest.skipIf(type_storage == 'db', "No apply for db")
+    def test_create3(self):
+        """Test for create command 2
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            state = State(name='Poloombia')
+            command = "create City name=\"Tex_as\" state_id=\"{}\"".format(
+                state.id)
+            HBNBCommand().onecmd(command)
+            _id = f.getvalue().strip()
+            key = "City" + "." + _id
+            nname = storage.all()[key].name
+            sid = storage.all()[key].state_id
+            self.assertTrue(key in storage.all().keys())
+            self.assertEqual('Tex as', nname)
+            self.assertEqual(state.id, sid)
+            self.assertEqual('Poloombia', state.name)
