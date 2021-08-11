fc5f137727d5c4cfa58f8d3b59141bca2d8fca17
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-08-10 22:32:12 -0500

    test6

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,58 +1,73 @@
 #!/usr/bin/python3
-"""
-===============================================================================
-
-████████╗███████╗███████╗████████╗     ██████╗ █████╗ ███████╗███████╗███████╗
-╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝    ██╔════╝██╔══██╗██╔════╝██╔════╝██╔════╝
-   ██║   █████╗  ███████╗   ██║       ██║     ███████║███████╗█████╗  ███████╗
-   ██║   ██╔══╝  ╚════██║   ██║       ██║     ██╔══██║╚════██║██╔══╝  ╚════██║
-   ██║   ███████╗███████║   ██║       ╚██████╗██║  ██║███████║███████╗███████║
-   ╚═╝   ╚══════╝╚══════╝   ╚═╝        ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
-
-===============================================================================
-"""
-
-from models.base_model import BaseModel
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+from models.base_model import BaseModel
 import unittest
-import json
-import pep8
-import datetime
+import os
+
+type_storage = os.getenv('HBNB_TYPE_STORAGE')
+
+
+class test_City(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "City"
+        self.value = City
+
+    @unittest.skipIf(type_storage == 'db', "No apply for db")
+    def test_state_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.state_id), str)
+
+    @unittest.skipIf(type_storage == 'db', "No apply for db")
+    def test_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
 
 
 class TestCity(unittest.TestCase):
-    """ Test City class implementation. """
-    def test_doc_module(self):
-        """ Module documentation. """
-        doc = City.__doc__
-        self.assertGreater(len(doc), 1)
 
-    def test_pep8_conformance_city(self):
-        """ Test that models/city.py conforms to PEP8. """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/city.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    @classmethod
+    def setUpClass(cls):
+        cls.city1 = City()
+        cls.city1.name = "San Francisco"
+        cls.city1.state_id = "CA"
 
-    def test_pep8_conformance_test_city(self):
-        """
-        - Test that tests/test_models/test_city.py conforms to PEP8.
-        """
-        pep8style = pep8.StyleGuide(quiet=True)
-        res = pep8style.check_files(['tests/test_models/test_city.py'])
-        self.assertEqual(res.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    @classmethod
+    def tearDownClass(cls):
+        del cls.city1
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def test_doc_constructor(self):
-        """ Constructor documentation. """
-        doc = City.__init__.__doc__
-        self.assertGreater(len(doc), 1)
+    def test_is_subclass(self):
+        self.assertTrue(issubclass(self.city1.__class__, BaseModel), True)
 
-    def test_class(self):
-        """ Validate the types of the attributes an class. """
-        with self.subTest(msg='Inheritance'):
-            self.assertTrue(issubclass(City, BaseModel))
+    def test_checking_for_functions(self):
+        self.assertIsNotNone(City.__doc__)
 
-        with self.subTest(msg='Attributes'):
-            self.assertIsInstance(City.name, str)
-            self.assertIsInstance(City.state_id, str)
+    def test_has_attributes(self):
+        self.assertTrue('id' in self.city1.__dict__)
+        self.assertTrue('created_at' in self.city1.__dict__)
+        self.assertTrue('updated_at' in self.city1.__dict__)
+        self.assertTrue('state_id' in self.city1.__dict__)
+        self.assertTrue('name' in self.city1.__dict__)
+
+    def test_attributes_are_strings(self):
+        self.assertEqual(type(self.city1.name), str)
+        self.assertEqual(type(self.city1.state_id), str)
+
+    @unittest.skipIf(type_storage == 'db', "not for db")
+    def test_save(self):
+        self.city1.save()
+        self.assertNotEqual(self.city1.created_at, self.city1.updated_at)
+
+    def test_to_dict(self):
+        self.assertEqual('to_dict' in dir(self.city1), True)
