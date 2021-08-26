1d40937c3943aa969e910035ee1acc2d616ddfea
jj131204 <jj131204@gmail.com>
2021-08-26 17:43:35 +0000

    new

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,8 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 echo "new html" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
