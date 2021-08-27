533e1d49876f80c1bf4ca6451300a5e8b9e9454e
jj131204 <jj131204@gmail.com>
2021-08-27 00:57:30 +0000

    test3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,14 +26,19 @@ def do_deploy(archive_path):
     complete = directory + file_
 
     try:
+
         put(archive_path, '/tmp')
         run('mkdir -p {}'.format(complete))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(file_, complete))
 
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(file_, complete))
         run('rm -f /tmp/{}.tgz'.format(file_))
 
+        run('mv {}/web_static/* {}/'.format(complete, complete))
+
+        run('rm -rf {}/web_static'.format(complete))
         run('rm -rf /data/web_static/current')
-        run('ln -s  {} /data/web_static/current'.format(complete))
+
+        run('ln -s {} /data/web_static/current'.format(complete))
 
         return True
 
