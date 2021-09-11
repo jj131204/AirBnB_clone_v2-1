74b8af4d2292bdb8dc606c830f3d9839f3741965
root <root@LAPTOP-4MQ15EMS.localdomain>
2021-09-10 21:12:47 -0500

    test

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,10 +35,10 @@ def number_(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def template_(n):
-    """display a HTML page only if n is an integer"""
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """" display a HTML page only if n is an integer"""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == '__main__':
