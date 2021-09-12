535a2832ad306261e7ed1b7d4303d71990cb17a6
root <root@LAPTOP-4MQ15EMS.localdomain>
2021-09-12 09:44:13 -0500

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """web_flask"""
-from flask import Flask
+"""from flask import Flask"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -36,10 +37,10 @@ def number_(n):
     return '{:d} is a number'.format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    """" display a HTML page only if n is an integer"""
-    return render_template('5-number.html', n=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def template_(n):
+    """ display a HTML page only if n is an integer"""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == '__main__':
