d7a39adc4e6a79af59e0502243898d09c927ff56
root <root@LAPTOP-4MQ15EMS.localdomain>
2021-09-12 09:29:38 -0500

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def c(text):
     return ('C ' + text.replace('_', ' '))
 
 
-@app.route('/python')
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_(text='is cool'):
     """" display “Python ”, followed by the value of the text variable"""
